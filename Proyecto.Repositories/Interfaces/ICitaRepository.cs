8b6889c702e66fe9fb9df09388e07c16760ba8e8
cesar <cesar-andree-2003@hotmail.com>
2024-09-09 11:12:19 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto.Repositories/Interfaces/ICitaRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Proyecto.Models;
+
+namespace Proyecto.Repositories.Interfaces;
+
+public interface ICitaRepository: IRepositoryBase<Cita>
+{
+    void Actualizar(Cita cita);
+
+    IEnumerable<SelectListItem> ObtenerTodosDropdownLista(string obj);
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Especialista es = new Especialista();
+            es.FirstName = "TestNombre1";
+            es.LastName = "Apellido1";
+            es.CodigoColegiatura = "A0001";
+            es.Especialidad = "Psicologo";
+            es.Info = "Info1";
+            es.Status true;
+        }
+    }
+}
